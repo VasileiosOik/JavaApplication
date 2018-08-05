f23d8b6746cd170aa9ff9ae7b7357fe2f33d27a9
VasileiosOik <econom07@hotmail.com>
2018-08-05 21:41:41 +0100

    refactoring

--- src/main/java/com/connection/configuration/MyBatisCompatabilityConfig.java
+++ src/main/java/com/connection/configuration/MyBatisCompatabilityConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class SpringBootMyBatisCompatabilityConfig {
+public class MyBatisCompatabilityConfig {
 
     @Autowired
     private SqlSessionFactory companySqlSessionFactory;
